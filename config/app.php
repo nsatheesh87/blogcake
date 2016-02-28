aa4c9e80f3bd2a1d1c21de94d6efa779b8dd2543
Satheesh Narayanan <nsatheesh87@gmail.com>
2016-02-28 18:05:52 +0000

    rest 1

--- config/app.php
+++ config/app.php
@@ -225,9 +225,9 @@ return [
              * the following line and set the port accordingly
              */ 
             //'port' => 'non_standard_port_number',
-            'username' => 'dbuser',
-            'password' => '123',
-            'database' => 'my_app',
+            'username' => 'adminl4zy1wf',
+            'password' => 'tPFfa9w2GlAG',
+            'database' => 'blogcake',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
